64a6513d3d95c0567a14fbaa50ee5e4fde88485c
David Mejia <david7mejia@gmail.com>
2021-03-05 17:45:27 -0500

    third commit

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,7 +1,13 @@
 let playerSym = 'x';
-let squareValues = ['', '', '','', '', '','', '', '']
+let squareValues = [
+    '', '', '',
+    '', '', '',
+    '', '', ''
+]
 
 window.addEventListener('DOMContentLoaded', event => {
+
+    let winner = ''
 
 
     document.getElementById('tic-tac-toe-board')
@@ -16,59 +22,111 @@ window.addEventListener('DOMContentLoaded', event => {
         img.src = `https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-${playerSym}.svg`
         event.target.appendChild(img)
         squareValues[sqIndex] = playerSym
-        if(playerSym === 'x'){
+        if (playerSym === 'x') {
             playerSym = 'o'
         } else {
             playerSym = 'x'
         }
+        if (rows() || columns() || diagnals()) {
+            let newArr = [];
+            squareValues.forEach(element => {
+                if (element.length > 0) {
+                    newArr.push(element)
+                }
+            });
+            if (newArr.length === 0) {
+                return
+            }
+            if (newArr.length >= 5) {
+                document.getElementById('game-status').innerHTML = `Winner is: ${winner}!`
 
+            }
+        } else {
+            let arr = []
+            squareValues.forEach(ele => {
+                if (ele.length > 0) {
+                    arr.push(ele)
+                }
+            })
+
+            if (arr.length === 9) {
+                document.getElementById('game-status').innerHTML = "It's a tie!"
+            }
+        }
     })
-    // if(columns() || rows() || diagnals()){
 
-    // }
-    // else {
-    //     let arr = []
-    //     squareValues.forEach(ele => {
-    //         if (ele.length > 0){
-    //             arr.push(ele)
-    //         }
-    //     })
-    //     if (arr.length === 9){
-    //         document.getElementById('game-status').innerHTML = "It's a tie!"
-    //     }
-    // }
-    // function rows(){
-    //     if (squareValues[0] === squareValues[1] && squareValues[1] === squareValues[2]){
-    //         return true
-    //     }
-    //     if (squareValues[3] === squareValues[4] && squareValues[4] === squareValues[5]){
-    //         return true
-    //     }
-    //     if (squareValues[6] === squareValues[7] && squareValues[7] === squareValues[8]){
-    //         return true
-    //     }
-    //     return false
-    // }
+    function rows() {
+        let newArr = [];
 
-    // function columns(){
-    //     if (squareValues[0] === squareValues[3] && squareValues[3] === squareValues[6]){
-    //         return true
-    //     }
-    //     if (squareValues[1] === squareValues[4] && squareValues[4] === squareValues[7]){
-    //         return true
-    //     }
-    //     if (squareValues[2] === squareValues[5] && squareValues[5] === squareValues[8]){
-    //         return true
-    //     }
-    //     return false
-    // }
-    // function diagnals(){
-    //     if (squareValues[0] === squareValues[4] && squareValues[4] === squareValues[8]){
-    //         return true
-    //     }
-    //     if (squareValues[2] === squareValues[4] && squareValues[4] === squareValues[6]){
-    //         return true
-    //     }
-    //     return false
-    // }
+        squareValues.forEach(element => {
+            if (element.length > 0) {
+                newArr.push(element)
+            }
+        });
+        if (newArr.length === 0) {
+            return false
+        }
+
+        if (squareValues[0] === squareValues[1] && squareValues[1] === squareValues[2]) {
+
+            if (squareValues[0].length > 0) {
+                winner = squareValues[0]
+                return true;
+            }
+        }
+        if (squareValues[3] === squareValues[4] && squareValues[4] === squareValues[5]) {
+            if (squareValues[3].length > 0) return true;
+        }
+        if (squareValues[6] === squareValues[7] && squareValues[7] === squareValues[8]) {
+            if (squareValues[6].length > 0) return true;
+        }
+
+        return false
+    }
+
+    function columns() {
+        let newArr = [];
+
+        squareValues.forEach(element => {
+            if (element.length > 0) {
+                newArr.push(element)
+            }
+        });
+        if (newArr.length === 0) {
+            return false
+        }
+        if (squareValues[0] === squareValues[3] && squareValues[3] === squareValues[6]) {
+            if (squareValues[0].length > 0) return true;
+        }
+        if (squareValues[1] === squareValues[4] && squareValues[4] === squareValues[7]) {
+            if (squareValues[1].length > 0) return true;
+        }
+        if (squareValues[2] === squareValues[5] && squareValues[5] === squareValues[8]) {
+            if (squareValues[2].length > 0) return true;
+        }
+        return false
+    }
+    function diagnals() {
+        let newArr = [];
+
+        squareValues.forEach(element => {
+            if (element.length > 0) {
+                newArr.push(element)
+            }
+        });
+        if (newArr.length === 0) {
+            return false
+        }
+        if (newArr.length === 0) {
+            return false
+        }
+        if (squareValues[0] === squareValues[4] && squareValues[4] === squareValues[8]) {
+            if (squareValues[0].length > 0) return true;
+        }
+        if (squareValues[2] === squareValues[4] && squareValues[4] === squareValues[6]) {
+            if (squareValues[2].length > 0) return true;
+        }
+        return false
+    }
 })
+
